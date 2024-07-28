e05bcfa0ace0ebeb6055bf328cd30f6a291d9fe4
eyobtadele2705 <eyobtadele2705@gmail.com>
2024-07-28 14:10:39 +0300

    react fundamentals build to deployment on netflify

---
+++ src/books.js
@@ -0,0 +1,31 @@
+import img1 from './images/curious minds.jpg'
+import img2 from './images/atomic habits.jpg'
+import img3 from './images/the wedding people.jpg'
+import img4 from './images/never lie.jpg'
+
+export const books = [
+  {
+    id: 1,
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: img1,
+  },
+  {
+    id: 2,
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img2,
+  },
+  {
+    id: 3,
+    author: 'Alisson Espach',
+    title: 'The Wedding People',
+    img: img3,
+  },
+  {
+    id: 4,
+    author: 'Freida McFadden',
+    title: 'Never Lie',
+    img: img4,
+  },
+]
